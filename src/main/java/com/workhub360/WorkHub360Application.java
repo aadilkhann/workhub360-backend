467d276fb1fde2f5cfdf2301a822ff2f1c4476b0
Adil Khan <khan01970@gmail.com>
2024-11-15 20:42:50 +0530

    Project and Set-up

---
+++ src/main/java/com/workhub360/WorkHub360Application.java
@@ -0,0 +1,13 @@
+package com.workhub360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHub360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkHub360Application.class, args);
+	}
+
+}
